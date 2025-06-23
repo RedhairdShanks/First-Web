d0a6f53fd5a593e971328e6bdd011f5dd9d81ded
RedhairdShanks <145724216+RedhairdShanks@users.noreply.github.com>
2025-06-23 11:57:45 -0400

    Update Web1.0.js

--- Web1.0.js
+++ Web1.0.js
@@ -11,3 +11,14 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+/*Burger bar*/
+function showSidebar(){
+  const sidebar= document.querySelector('.sidebar')
+  sidebar.style.display = 'flex' /* Show the sidebar */
+}
+
+function hideSidebar() {
+  const sidebar = document.querySelector('.sidebar')
+  sidebar.style.display = 'none' /* Hides the sidebar */
+}
